b333ac668d69d43e2d9fe75602bd4f1f90c00e13
Max Marrone <max@marrone.nyc>
2023-04-30 19:48:56 -0400

    Delete obsolete EC2 provisioning variables.

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,15 +1,1 @@
-variable "host" {
-  type        = string
-  description = "The DNS name by which this instance can be reached over the public Internet. This is required so TLS certificates can be automatically issued and renewed."
-}
-
-variable "email" {
-  type        = string
-  description = "Your email, for certificate authorities to contact you if something goes wrong."
-}
-
-variable "tls_staging" {
-  type        = bool
-  default     = false
-  description = "Whether to use test certificates instead of real ones. Enable this during debugging to avoid getting rate-limited by the certificate authority."
-}
+# This module has no input variables.
